fd693182be90bf67861c00cc5c72cc7066024220
Kay <kayxn23@gmail.com>
2019-01-05 20:28:47 -0800

    added rest controller actions in Game controller

--- src/main/java/com/capstonebackend/controllers/GameController.java
+++ src/main/java/com/capstonebackend/controllers/GameController.java
@@ -3,28 +3,65 @@ package com.capstonebackend.controllers;
 
 import com.capstonebackend.input.GameCreateInput;
 import com.capstonebackend.models.Game;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.capstonebackend.repositories.GameRepository;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 //@ these are annotations and they're always scoped
 @RestController
 public class GameController {
 
-    @GetMapping("/games")
-    public List<Game> getAllGames() {
-        return Arrays.asList(
-                new Game("Organizer1", "Location1"));
+    private final GameRepository repository;
+
+    GameController(GameRepository repository) {
+        this.repository = repository;
     }
 
+    //Aggregate root
 
+    @GetMapping("/games")
+    public List<Game> all() {
+                return repository.findAll();
+    }
 
     @PostMapping("/games")
-    public Game create(@RequestBody GameCreateInput gameDetails) {
-        return new Game(gameDetails.getOrganizer(), gameDetails.getLocation());
+    Game newGame(@RequestBody Game newGame) {
+        return repository.save(newGame);
     }
+
+//    @PostMapping("/games")
+//    public Game create(@RequestBody GameCreateInput gameDetails) {
+//        return new Game(gameDetails.getOrganizer(), gameDetails.getLocation());
+//    }
+
+    //Single item
+
+    @GetMapping("/games/{id}")
+    Game one(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new GameNotFoundException(id));
+    }
+
+    @PutMapping("/games/{id}")
+    Game replaceGame(@RequestBody Game newGame, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(game -> {
+                    game.setLocation(newGame.getLocation());
+                    game.setOrganizer(newGame.getOrganizer());
+                    return repository.save(game);
+                })
+                .orElseGet(() -> {
+                    newGame.setId(id);
+                    return repository.save(newGame);
+                });
+    }
+
+    @DeleteMapping("/games/{id")
+    void deleteGame(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+
+
 }
