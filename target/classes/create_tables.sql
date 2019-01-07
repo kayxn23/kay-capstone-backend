a01e8b8fb61b65183459d4b0f0d5d7cd3e7687c8
Kay <kayxn23@gmail.com>
2019-01-06 18:33:15 -0800

    deleted LoadDatabase class now that i dont need db to preload with test data

--- target/classes/create_tables.sql
+++ target/classes/create_tables.sql
@@ -1,8 +1,4 @@
-CREATE TABLE game(
-    id BIGINT PRIMARY KEY     NOT NULL,
-    organizer VARCHAR(20),
-    location VARCHAR(20)
-);
+
 
 1. create database capstonebackend
 2. \connect capstonebackend
@@ -12,4 +8,4 @@ CREATE TABLE game(
        location VARCHAR(20)
    );
 4. \dt
-5. select * from game;    (read rows )
+5. select * from game;    //(show all the data in this table named game )
