8b857dee7884d7f6e23b8e90811e9de36e6c60c5
Kay <kayxn23@gmail.com>
2019-01-11 14:06:50 -0800

    added @jsonignore to stop infiniate recursion in location model

--- src/main/java/com/capstonebackend/models/Location.java
+++ src/main/java/com/capstonebackend/models/Location.java
@@ -1,10 +1,12 @@
 package com.capstonebackend.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Set;
 
 @Data
@@ -15,8 +17,11 @@ public class Location implements Serializable {
 
     //mappedBy property is what we use to tell Hibernate which variable we are using to represent the parent class in our child class.
     @OneToMany(mappedBy="location")
-    private Set<Game> game; //think if this should be plural
-//one location to many games
+    @JsonIgnore
+//    private Set<Game> games; //think if this should be plural
+    private List<Game> games;
+
+    //one location to many games
     protected Location(){}
 
     private static final long serialVersionUID = 1L;
