6df0202d02f5de083dcabcd6b2bd516b48e06e7e
Kay <kayxn23@gmail.com>
2019-01-22 20:55:47 -0800

    committed location data to db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,61 @@
-INSERT INTO location(latitude,longitude, location_title) VALUES ('47.550259','-122.264847');
-INSERT INTO location(latitude,longitude, location_title) VALUES ('47.721790','-122.327209');
-INSERT INTO location(latitude,longitude, location_title) VALUES ('47.620598','-122.361816');
-INSERT INTO location(latitude,longitude) VALUES ('47.563450','-122.404251');
-INSERT INTO location(latitude,longitude) VALUES ('47.622120','-122.284554');
-INSERT INTO location(latitude,longitude) VALUES ('47.664944','-122.401909');
-INSERT INTO location(latitude,longitude) VALUES ('47.627316','-122.366882');
-INSERT INTO location(latitude,longitude) VALUES ('47.635471','-122.308403');
-INSERT INTO location(latitude,longitude) VALUES ('47.689709','-122.402328');
-INSERT INTO location(latitude,longitude) VALUES ('47.564445','-122.278511');
-INSERT INTO location(latitude,longitude) VALUES ('47.681568','-122.341133');
-INSERT INTO location(latitude,longitude) VALUES ('47.645630','-122.334351');
-INSERT INTO location(latitude,longitude) VALUES ('47.599220','-122.290833');
-INSERT INTO location(latitude,longitude) VALUES ('47.661133','-122.356567');
-INSERT INTO location(latitude,longitude) VALUES ('47.593296','-122.386024');
-INSERT INTO location(latitude,longitude) VALUES ('47.538376','-122.289772');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.579098','-122.411263', 'Alki Beach Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.614506','-122.288727', 'Alvin Larkins Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.524395','-122.263405','Beer Sheva Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.617012','-122.319130','Cal Anderson Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.621708','-122.331985','Cascade Playground');
 
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.617012','-122.319130','Cal Anderson Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.601982	','-122.330429','City Hall Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.586460','-122.290047','Colman Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.619053','-122.340935','Denny Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.655598','-122.410347','Denny-Blaine Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.586460','-122.290047','Discovery Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.663540','-122.269333','East Montlake Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.521023','-122.383171','Fauntleroy Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.609306','-122.330902','Freeway Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.599220','-122.290833','Colman Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.645630','-122.334351','Gas Works Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.564445	','-122.278511','Genesee Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.689709	','-122.402328','Golden Gardens Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.681568','-122.341133','Green Lake Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.593296','-122.386024','Hamilton Viewpoint');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.598530','-122.325371','Hing Hay Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.568794','-122.310310','Jefferson Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.627316','-122.366882','Kinnear Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.569180','-122.291954','Lake People Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.600777','-122.288071','Leschi Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.529198','-122.396774','Lincoln Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.635647','-122.277260','Madison Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.610378','-122.283661','Madrona Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.711624','-122.372169','Magnuson Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.695797','-122.274033','Matthews Beach Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.563450','-122.404251','Me-Kwa-Mooks Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.641953','-122.309174','Montlake Playfield');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.721790','-122.327209','Northacres Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.544964','-122.321602','Oxbow Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.655724','-122.319443','Peace Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.672462','-122.308037','Ravenna Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.643578','-122.320183','Roanoke Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.550259	','-122.264847','Seward Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.618462','-122.347473','Tilikum Place');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.610149','-122.343796','Victor Steinbrueck Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.619240','-122.282158','Viretta Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.630127','-122.314636','Volunteer Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.639793','-122.294472','Washington Park Arboretum');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.607185','-122.341995','Waterfront Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.610954','-122.337013','Westlake Park');
+
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.610954','-122.337013','Westlake Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.646030','-122.309448','West Montlake Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.610954','-122.337013','Westlake Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.616512','-122.355507','Olympic Sculpture Park');
+INSERT INTO location(latitude,longitude, location_title) VALUES ('47.610954','-122.337013','Westlake Park');
 
 
